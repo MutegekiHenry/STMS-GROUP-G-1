55895077f47a8f17687c93557a915b15d5b78d8c
MutegekiHenry <henrymutegeki117@gmail.com>
2021-01-12 11:58:49 +0300

    chore: refactor code

--- Group g-1/ideal_client.c
+++ Group g-1/ideal_client.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
     //server.sin_addr.s_addr = inet_addr(argv[1]);
  // server.sin_port        = htons(port_no);
   server.sin_addr.s_addr = inet_addr("127.0.0.1");
-   server.sin_port        = htons(9006);
+  server.sin_port        = htons(9006);
   server.sin_family      = AF_INET;
     //Connect to remote server
     if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
